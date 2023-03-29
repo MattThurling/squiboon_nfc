07eb173bacad11b9de57d6dd7feddd8ac8ed44e8
Matt Thurling <mthurling@icloud.com>
2023-03-29 12:41:58 +0100

    phases

--- src/views/Question.vue
+++ src/views/Question.vue
@@ -1,19 +1,19 @@
 <template>
   <div class="h-screen flex flex-col justify-center items-center py-12 px-4">
     <p class="text-4xl text-center mb-12">
-      {{ question.body }}
+      {{ question.body }}<br>{{ phase }}<br>{{ profile }}
     </p>
-    <div class="">
+    <button v-if="phase ==0" class="btn btn-lg btn-outline btn-primary mx-4" @click="scan">Vote Now</button>
+    <button v-if="phase ==0" class="btn btn-lg btn-outline btn-primary mx-4" @click="getProfile">Get Profile</button>
+    <div v-if="phase == 1">
       <p class="text-2xl text-center mb-12">
         {{ info }}
       </p>
       <p class="text-2xl text-center mb-12">
         {{ sId }}
       </p>
-      <button class="btn btn-lg btn-primary mx-4" @click="scan">Scan</button>
     </div>
-    <div class="mt-8">
-      
+    <div v-if="phase == 2" class="mt-8">
       <button class="btn btn-lg btn-outline btn-primary mx-4" @click="">
         Yes
       </button>
@@ -31,23 +31,30 @@
   import { nfc } from '../nfc'
 
   const question = ref({id: null, body: null})
+  const profile = ref({id: null, name: null, nfc: null})
   const route = useRoute()
   const sId = ref('---')
   const info = ref('Press the button to scan your NFC tag.')
+  const phase = ref(0)
 
   onMounted(async() => {
     let response = await axios.get(`https://squiboon.nw.r.appspot.com/api/questions/${route.params.id}`)
-    scan()
     question.value = response.data
   })
 
+  const getProfile = async () => {
+    let u = encodeURIComponent(sId.value)
+    console.log(u)
+    let response = await axios.get(`https://squiboon.nw.r.appspot.com/api/profiles/nfc/${u}`)
+    profile.value = response.data
+  }
 
-  
   const scan = async () => {
     console.log('scan')
     try {
 
       await nfc.scan()
+      phase.value++
       console.log('> Scan started')
       info.value = 'Scan started. Hold your NFC tag near the device.'
 
@@ -56,15 +63,19 @@
           sId.value = e.serialNumber
           console.log(`> Serial Number: ${e.serialNumber}`)
           console.log(`> Records: (${e.message.records.length})`)
+          phase.value++
         },
+      }
+      if (sId.value != '---') {
+        getProfile()
       }
 
       nfc.addEventListener('reading', listenerObject)
       nfc.addEventListener('readingerror', () => {
-        console.log('Argh! Cannot read data from the NFC tag. Try another one?')
+        alert('Argh! Cannot read data from the NFC tag. Is NFC reading enabled on device?')
       })
     } catch (error) {
-      console.log('Argh! ' + error)
+      alert('Argh! ' + error)
     }
   }
 </script>
